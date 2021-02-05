f4e3c232dca274c1cde021b5e89c0b1cfe4741e3
AbdusadikSattar <71940275+AbdusadikSattar@users.noreply.github.com>
2021-02-05 18:36:54 +0100

    Abdusadik commit something

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 //        },
         features = "src/test/resources/features",
         glue = "com/zerobank/step_definitions",
-        dryRun = true,
+        dryRun = true,//abdusadik Sattar is here
         tags = "@wip"
 
 )
